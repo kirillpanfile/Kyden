acfada264005f3f673ea246a2c947084514f0d3e
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-04-30 18:00:10 +0300

    Init

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template></template>
+
+<style lang="scss">
+html {
+  line-height: 1.15;
+  -webkit-text-size-adjust: 100%;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+}
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #111111;
+}
+#app {
+  height: 100vh;
+  background-image: url("/src/assets/pattern bg.png");
+  opacity: 0.03;
+  z-index: -1;
+}
+</style>
