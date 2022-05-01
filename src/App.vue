04a05c948e861cff8eed422f6949cfd34f1d233a
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-05-01 18:58:17 +0300

    AppButton created + bg fix

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <ul id="users">
-    <li></li>
-  </ul>
+  <router-view>
+    <router-link to="/">Home</router-link>
+    <app-button>Hello</app-button>
+  </router-view>
 </template>
 
 <script>
 export default {
   name: "app",
-  mounted: () => {
+  mounted() {
     this.$store.dispatch("getProducts");
   },
 };
@@ -25,14 +26,31 @@ html {
   box-sizing: border-box;
 }
 body {
-  margin: 0;
-  padding: 0;
+  position: relative;
+  padding-bottom: 0;
+  width: 100%;
+  min-height: 100%;
   background-color: #111111;
 }
-#app {
-  height: 100vh;
+button {
+  border: none;
+  box-shadow: none;
+  outline: none;
+  -webkit-appearance: none;
+  background: none;
+  cursor: pointer;
+}
+#bg {
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0.05;
+  z-index: -1;
   background-image: url("/src/assets/pattern bg.png");
   opacity: 0.03;
+  // background-color: rgba(0, 0, 0, 1);
   z-index: -1;
 }
 </style>
---
+++ src/components/UI/AppButton.vue
@@ -0,0 +1,48 @@
+<template>
+  <button class="btn btn--primary">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "app-button",
+  props: {
+    type: {
+      type: String,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.btn {
+  font-family: "Roboto Condensed", sans-serif;
+  line-height: 1.2;
+  font-weight: 700;
+  text-transform: uppercase;
+  letter-spacing: 0px;
+  display: inline-block;
+  transition: opacity 200ms, border-color 200ms, background-color 200ms,
+    color 200ms;
+  border: 2px solid transparent;
+  border-radius: 0px;
+  padding: 17px 20px 16px;
+  cursor: pointer;
+  font-size: 0.95rem;
+  line-height: 1.2;
+  text-align: center;
+}
+
+.btn--expand {
+  display: block;
+  width: 100%;
+}
+.btn--primary {
+  background-color: #cd412b;
+  color: #f6eae1;
+}
+.btn:hover {
+  opacity: 0.8;
+}
+</style>
