d8d6e424f9c5e8b7d4eb4ccf228c10e3b674d6a2
Panfile Kirill <95650362+kirillpanfile@users.noreply.github.com>
2022-05-06 19:49:45 +0300

    error commit

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,3 @@
 module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset'],
-  plugins: [
-    [
-      '@babel/plugin-transform-runtime',
-      {
-        corejs: 3,
-        helpers: true,
-        regenerator: true,
-        useESModules: false
-      }
-    ]
-  ]
+  presets: ['@vue/cli-plugin-babel/preset']
 }
